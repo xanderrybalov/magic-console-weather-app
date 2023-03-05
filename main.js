d2473daa587a7ff6cb57834756ad8d217867757e
Xander Rybalov <xanderrybalov@gmail.com>
2023-03-05 20:51:07 +0200

    add core logic of args for console

--- main.js
+++ main.js
@@ -4,6 +4,16 @@ import { getArgs } from './helpers/args.js';
 const initCLI = () => {
 	const args = getArgs(process.argv);
 	console.log(args);
+	if(args.h) {
+		// show help
+	};
+	if (args.s) {
+		// save the city
+	};
+	if (args.t) {
+		// save token
+	};
+	// show weather
 };
 
 initCLI();
